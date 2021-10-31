8a60321e32f003bf8d8f54373d7e01ca938d4135
ashlchen <ashlchen@iu.edu>
2021-10-31 15:14:35 -0400

    Finish Referee view function

---
+++ app/public/api/GameAssignment/index.php
@@ -0,0 +1,31 @@
+<?php
+require 'class/DbConnection.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM GameAssignment';
+$vars = [];
+
+if (isset($_GET['referee'])) {
+  // This is an example of a parameterized query
+  $sql = 'SELECT * FROM GameAssingment WHERE RefereeID = ?';
+
+  //NOT THIS WAY
+  // $sql = 'SELECT * FROM offer WHERE studentId = ' . $_GET['student'];
+
+  $vars = [ $_GET['referee'] ];
+}
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$referee = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($referee, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
